c9af0cd57e538ab3588099f0336abed874beff0b
Abyan Rizqo <rizqo1412@gmail.com>
2021-10-18 10:47:18 +0700

    Change form view

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,139 +1,58 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>{{$title}}</title>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preview surat lowongan</title>
     <style>
-      html, body {
-      height: 100%;
-      }
-      body, input, select { 
-        background: #999;
-      padding: 0;
-      margin: 0;
-      outline: none;
-      font-family: Roboto, Arial, sans-serif;
-      font-size: 16px;
-      color: #eee;
-      }
-      h1, h3 {
-      font-weight: 400;
-      }
-      h1 {
-      font-size: 32px;
-      }
-      h3 {
-      color: #1c87c9;
-      }
-      .main-block, .info {
-      display: relative;
-      }
-      .main-block {
-      justify-content: center;
-      align-items: center;
-      width: 50%; 
-      margin:0 auto;
-      min-height: 100%;
-      background: #999;
-      background-size: cover;
-      }
-      form {
-      width: 80%; 
-      padding: 25px;
-      margin-bottom: 20px;
-      background: rgba(0, 0, 0, 0.9);
-      }
-      input, select {
-      padding: 5px;
-      margin-bottom: 20px;
-      background: transparent;
-      border: none;
-      border-bottom: 1px solid #eee;
-      }
-      input::placeholder {
-      color: #eee;
-      }
-      option {
-      background: black; 
-      border: none;
-      }
-      .metod {
-      display: flex; 
-      }
-      input[type=radio] {
-      display: none;
-      }
-      label.radio {
-      position: relative;
-      display: inline-block;
-      margin-right: 20px;
-      text-indent: 32px;
-      cursor: pointer;
-      }
-      label.radio:before {
-      content: "";
-      position: absolute;
-      top: -1px;
-      left: 0;
-      width: 17px;
-      height: 17px;
-      border-radius: 50%;
-      border: 2px solid #1c87c9;
-      }
-      label.radio:after {
-      content: "";
-      position: absolute;
-      width: 8px;
-      height: 4px;
-      top: 5px;
-      left: 5px;
-      border-bottom: 3px solid #1c87c9;
-      border-left: 3px solid #1c87c9;
-      transform: rotate(-45deg);
-      opacity: 0;
-      }
-      input[type=radio]:checked + label:after {
-      opacity: 1;
-      }
-      button {
-      display: block;
-      width: 200px;
-      padding: 10px;
-      margin: 20px auto 0;
-      border: none;
-      border-radius: 5px; 
-      background: #1c87c9; 
-      font-size: 14px;
-      font-weight: 600;
-      color: #fff;
-      }
-      button:hover {
-      background: #095484;
-      }
-      @media (min-width: 568px) {
-      .info {
-      flex-flow: row wrap;
-      justify-content: space-between;
-      }
-      input {
-      width: 100%;
-      }
-      textarea {
-      color:black;
-      }
-      input.fname {
-      width: 100%;
-      }
-      select {
-      width: 48%;
-      }
-      }
+        .main {
+            display: relative;
+            position: center;
+            margin:0 auto;
+            flex-direction: column;
+            width:50%;
+            font-size:16px;
+        }
+        .main2 {
+            padding:3px;
+            border: 1px solid #000000;
+        }
+        .button {
+            font-size: 16px;
+            letter-spacing: 2px;
+            font-weight: 400 !important;
+            background-color: #e63946ff;
+            color: #fff;
+            padding: 3px 5px;
+            margin: auto;
+            text-align: center;
+            display: inline-block !important;
+            text-decoration: none;
+            border: 0px;
+            width: max-content;
+            cursor: pointer;
+            transition: all 0.3s 0s ease-in-out;
+        }
+        input {
+            width: 420px;
+            height:20px;
+            margin-bottom:3px;
+        }
+
+        .button:hover {
+            transform: scale(1.04, 1);
+            background-color: rgb(216, 10, 27);
+            text-decoration: none;
+        }
+        .button a{
+            color:#fff;
+        }
     </style>
-  </head>
-  <body>
-    <div class="main-block">
-      <h1>{{$title}}</h1>
+</head>
+<body>
+    <div class="main">
+    <h1>{{$title}}</h1>
       <form action="{{$to}}" method="post">
       @csrf
         <div class="info">
@@ -148,5 +67,8 @@
         <button class="button" type="submit" value="Submit">Preview</button>
       </form>
     </div>
-  </body>
+    </div>
+    
+</p>
+</body>
 </html>
